b52ebd85c91535fc72d5577f8bc9156fa0d3e244
Gustavo <gustavosousa4424@gmail.com>
2024-11-04 14:29:57 -0300

    Update calculadoraip.py
    Implementação da função calcularEnderecoBroadcast
    
    - Adiciona a função calcularEnderecoBroadcast à classe CalculadoraIp.
    - A função calcula o endereço de broadcast a partir de um endereço IP e uma máscara de sub-rede.
    - Realiza a conversão do IP e da máscara de sub-rede de decimal para binário.
    - Executa a operação OR bit a bit entre cada octeto do IP e o complemento da máscara de sub-rede (invertendo seus bits).
    - Converte o resultado de volta para o formato decimal e retorna o endereço de broadcast correspondente.

--- calculadoraip.py
+++ calculadoraip.py
@@ -1,5 +1,6 @@
 #Calculadora Ip
 #Irei utilizar POO para fazer está calculadora
+#A interface será feita utilizando a bilioteca "Streamlit"
 
 class CalculadoraIp:
     def __init__(self, EnderecoIp: str, MascaraSubrede: str):
@@ -24,16 +25,25 @@ class CalculadoraIp:
     
     #Função para calcular o endereco de rede    
     def calcularEnderecoRede(self):
-        IpBin = self.IpParaBinario(self.EnderecoIp).split(".") #Separando os octetoscem listas
+        IpBin = self.IpParaBinario(self.EnderecoIp).split(".") #Separando os octetos em listas
         MascaraBin = self.MascaraSubredeParaBinario(self.MascaraSubrede).split(".") 
         EnderecoRedeBin = []
         for i in range(4):  #Como eu já sei que o IP e a máscara têm 4 octetos
             RedeOcteto = format(int(IpBin[i], 2) & int(MascaraBin[i], 2), '08b') #Aqui ta fazendo a operação AND bit a bit
             EnderecoRedeBin.append(RedeOcteto)
-        return ".".join(str(int(octeto, 2)) for octeto in EnderecoRedeBin) #Tá tranformando de binário pra decimal e juntando os octetos
-    
+        return ".".join(str(int(octeto, 2)) for octeto in EnderecoRedeBin) #Tá transformando de binário pra decimal e juntando os octetos
+
+    #Função para calcular o endereço broadcast, usei quase a mesma lógica do endereço de rede só mudou que uso "or = |" nvés de "and = &" e também a mascara é invertida
+    def calcularBroadcast(self):
+        IpBin = self.IpParaBinario(self.EnderecoIp).split(".") #Nessa parte eu uso a mesma lógica para separa os octetos
+        MascaraBin = self.MascaraSubredeParaBinario(self.MascaraSubrede).split(".")
+        BroadcastBin = []
+        for i in range(4):
+            BroadcastOcteto = format(int(IpBin[i], 2) | (int(MascaraBin[i], 2) ^ 0xFF), '08b') #Aqui a máscara é invetida (inverte os bits) e também é realizado a operação OR com o IP, uso o "0xFF" porque ele transforma os bits de rede em 0 e os bits de host em 1
+            BroadcastBin.append(BroadcastOcteto)
+        return ".".join(str(int(octeto, 2)) for octeto in BroadcastBin)
+
 c1 = CalculadoraIp("192.168.1.10", "255.255.255.0")
-print(f"IP em binário: {c1.IpParaBinario(c1.EnderecoIp)}")
-print(f"Máscara de subrede em binário: {c1.MascaraSubredeParaBinario(c1.MascaraSubrede)}")
 print(f"Endereço de rede: {c1.calcularEnderecoRede()}")
+print(f"Endereço Broadcast: {c1.calcularBroadcast()}")
 
