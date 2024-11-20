6f3e5e0ef2c34355a199bebf2bfdd2745f37e90a
Gustavo <gustavosousa4424@gmail.com>
2024-11-20 12:51:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     st.title("Calculadora Ip para IPV4") #Título da página
     st.write("A calculadora IP analisa o endereço IP e sua máscara, mostrando informações como o endereço de rede, converte a máscara para o formato CIDR, primeiro e último host, endereço de broadcast, classe do IP, número de sub-redes, hosts por sub-rede, se o IP é público ou privado e exibe o intervalo de IPs disponíveis.") #Pequena descrição das funções
     enderecoIp = st.text_input("Digite o endereço IP (Ex: 192.168.0.1):")  #Campo do ip
-    mascaraSubrede = st.text_input("Digite a máscara de sub-rede (Ex: 255.255.255.0):") #Campo da máscara
+    mascaraSubrede = st.text_input("Digite a máscara de sub-rede (Ex: 255.255.255.0 ou /24):") #Campo da máscara
     
     if st.button("Calcular"): #Botão que chama a classe "CalculadoraIp"
         if enderecoIp and mascaraSubrede:  #Verificaçã pra saber  se ambos dos campos estão preenchidos
